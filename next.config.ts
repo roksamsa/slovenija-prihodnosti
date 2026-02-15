80c06086c84327edecd22b0a21c3af5c0ffb44bd
Rok Samsa <rok.samsa@gmail.com>
2026-02-15 15:39:00 +0100

    Fix: Images fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "www.demokrati.si", pathname: "/wp-content/uploads/**" },
+      { protocol: "https", hostname: "www.sds.si", pathname: "/app/uploads/**" },
+      { protocol: "https", hostname: "gibanjesvoboda.si", pathname: "/wp-content/uploads/**" },
+      { protocol: "https", hostname: "www.levica.si", pathname: "/wp-content/uploads/**" },
+      { protocol: "https", hostname: "stranka-resnica.si", pathname: "/wp-content/uploads/**" },
+      { protocol: "https", hostname: "prerod.si", pathname: "/wp-content/uploads/**" },
+      { protocol: "https", hostname: "socialnidemokrati.si", pathname: "/wp-content/uploads/**" },
+      { protocol: "https", hostname: "upload.wikimedia.org", pathname: "/wikipedia/**" },
+      { protocol: "https", hostname: "piratskastranka.si", pathname: "/content/images/**" },
+    ],
+  },
 };
 
 export default nextConfig;
